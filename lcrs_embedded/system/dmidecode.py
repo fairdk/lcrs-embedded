5de1b68f694682265285a79929dae59aeba00f10
Pieter Vancraeyveld <pieter.vancraeyveld@gmail.com>
2017-07-05 18:02:12 +0200

    Get rid of the beautiful indentation again

--- lcrs_embedded/system/dmidecode.py
+++ lcrs_embedded/system/dmidecode.py
@@ -15,7 +15,6 @@ makes the presented information possibly unreliable.
 
 import logging
 import re
-import textwrap
 
 from .. import models
 from ..utils.decorators import run_command
@@ -64,21 +63,21 @@ def dmidecode_system(scan_result, stdout, stderr, succeeded):
     fetch_rows(scan_result, stdout, row_map)
 
 
-dmidecode_system.mock_output = textwrap.dedent("""\
-    # dmidecode 2.9
-    SMBIOS 2.4 present.
+dmidecode_system.mock_output = """
+# dmidecode 2.9
+SMBIOS 2.4 present.
 
-    Handle 0x0001, DMI type 1, 27 bytes
-    System Information
-            Manufacturer: LENOVO
-            Product Name: 1952W5R
-            Version: ThinkPad T60
-            Serial Number: 01234566
-            UUID: 01234567-1234-1234-1234-123456789ABC
-            Wake-up Type: Power Switch
-            SKU Number: Not Specified
-            Family: ThinkPad T60
-    """)
+Handle 0x0001, DMI type 1, 27 bytes
+System Information
+        Manufacturer: LENOVO
+        Product Name: 1952W5R
+        Version: ThinkPad T60
+        Serial Number: 01234566
+        UUID: 01234567-1234-1234-1234-123456789ABC
+        Wake-up Type: Power Switch
+        SKU Number: Not Specified
+        Family: ThinkPad T60
+"""
 dmidecode_system.expected_results = {
     'system_manufacturer': "LENOVO",
     'system_product_id': "1952W5R",
@@ -104,33 +103,32 @@ def dmidecode_baseboard(scan_result, stdout, stderr, succeeded):
 
     fetch_rows(scan_result, stdout, row_map)
 
+dmidecode_baseboard.mock_output = """
+# dmidecode 3.0
+Getting SMBIOS data from sysfs.
+SMBIOS 2.6 present.
 
-dmidecode_baseboard.mock_output = textwrap.dedent("""\
-    # dmidecode 3.0
-    Getting SMBIOS data from sysfs.
-    SMBIOS 2.6 present.
+Handle 0x000F, DMI type 2, 15 bytes
+Base Board Information
+    Manufacturer: LENOVO
+    Product Name: OVONEL
+    Version: Not Available
+    Serial Number: 12345678
+    Asset Tag: Not Available
+    Features:
+        Board is a hosting board
+        Board is replaceable
+    Location In Chassis: Not Available
+    Chassis Handle: 0x0000
+    Type: Motherboard
+    Contained Object Handles: 0
 
-    Handle 0x000F, DMI type 2, 15 bytes
-    Base Board Information
-        Manufacturer: LENOVO
-        Product Name: OVONEL
-        Version: Not Available
-        Serial Number: 12345678
-        Asset Tag: Not Available
-        Features:
-            Board is a hosting board
-            Board is replaceable
-        Location In Chassis: Not Available
-        Chassis Handle: 0x0000
-        Type: Motherboard
-        Contained Object Handles: 0
-
-    Handle 0x0029, DMI type 10, 6 bytes
-    On Board Device Information
-        Type: Other
-        Status: Disabled
-        Description: IBM Embedded Security hardware
-    """)
+Handle 0x0029, DMI type 10, 6 bytes
+On Board Device Information
+    Type: Other
+    Status: Disabled
+    Description: IBM Embedded Security hardware
+"""
 dmidecode_baseboard.expected_results = {
     'baseboard_manufacturer': "LENOVO",
     'baseboard_serial_number': "12345678",
@@ -153,29 +151,28 @@ def dmidecode_chassis(scan_result, stdout, stderr, succeeded):
 
     fetch_rows(scan_result, stdout, row_map)
 
+dmidecode_chassis.mock_output = """
+# dmidecode 3.0
+Getting SMBIOS data from sysfs.
+SMBIOS 2.6 present.
 
-dmidecode_chassis.mock_output = textwrap.dedent("""\
-    # dmidecode 3.0
-    Getting SMBIOS data from sysfs.
-    SMBIOS 2.6 present.
-
-    Handle 0x0010, DMI type 3, 21 bytes
-    Chassis Information
-        Manufacturer: LENOVO
-        Type: Notebook
-        Lock: Not Present
-        Version: Not Available
-        Serial Number: X123YZ
-        Asset Tag: No Asset Information
-        Boot-up State: Unknown
-        Power Supply State: Unknown
-        Thermal State: Unknown
-        Security Status: Unknown
-        OEM Information: 0x00000000
-        Height: Unspecified
-        Number Of Power Cords: Unspecified
-        Contained Elements: 0
-    """)
+Handle 0x0010, DMI type 3, 21 bytes
+Chassis Information
+    Manufacturer: LENOVO
+    Type: Notebook
+    Lock: Not Present
+    Version: Not Available
+    Serial Number: X123YZ
+    Asset Tag: No Asset Information
+    Boot-up State: Unknown
+    Power Supply State: Unknown
+    Thermal State: Unknown
+    Security Status: Unknown
+    OEM Information: 0x00000000
+    Height: Unspecified
+    Number Of Power Cords: Unspecified
+    Contained Elements: 0
+"""
 dmidecode_chassis.expected_results = {
     'chassis_manufacturer': "LENOVO",
     'chassis_type': "Notebook",
@@ -219,60 +216,60 @@ def dmidecode_memory(scan_result, stdout, stderr, succeeded):
     scan_result.memory_devices_count = len(scan_result.memory_devices)
 
 
-dmidecode_memory.mock_output = textwrap.dedent("""\
-    # dmidecode 3.0
-    Getting SMBIOS data from sysfs.
-    SMBIOS 2.6 present.
+dmidecode_memory.mock_output = """
+# dmidecode 3.0
+Getting SMBIOS data from sysfs.
+SMBIOS 2.6 present.
 
-    Handle 0x0005, DMI type 16, 15 bytes
-    Physical Memory Array
-        Location: System Board Or Motherboard
-        Use: System Memory
-        Error Correction Type: None
-        Maximum Capacity: 16 GB
-        Error Information Handle: Not Provided
-        Number Of Devices: 2
+Handle 0x0005, DMI type 16, 15 bytes
+Physical Memory Array
+    Location: System Board Or Motherboard
+    Use: System Memory
+    Error Correction Type: None
+    Maximum Capacity: 16 GB
+    Error Information Handle: Not Provided
+    Number Of Devices: 2
 
-    Handle 0x0006, DMI type 17, 28 bytes
-    Memory Device
-        Array Handle: 0x0005
-        Error Information Handle: Not Provided
-        Total Width: 64 bits
-        Data Width: 64 bits
-        Size: 4096 MB
-        Form Factor: SODIMM
-        Set: None
-        Locator: ChannelA-DIMM0
-        Bank Locator: BANK 0
-        Type: DDR3
-        Type Detail: Synchronous
-        Speed: 1333 MHz
-        Manufacturer: Samsung
-        Serial Number: 1234567
-        Asset Tag: 12345678910
-        Part Number: XYSADKLJADSL-132
-        Rank: Unknown
+Handle 0x0006, DMI type 17, 28 bytes
+Memory Device
+    Array Handle: 0x0005
+    Error Information Handle: Not Provided
+    Total Width: 64 bits
+    Data Width: 64 bits
+    Size: 4096 MB
+    Form Factor: SODIMM
+    Set: None
+    Locator: ChannelA-DIMM0
+    Bank Locator: BANK 0
+    Type: DDR3
+    Type Detail: Synchronous
+    Speed: 1333 MHz
+    Manufacturer: Samsung
+    Serial Number: 1234567
+    Asset Tag: 12345678910
+    Part Number: XYSADKLJADSL-132
+    Rank: Unknown
 
-    Handle 0x0007, DMI type 17, 28 bytes
-    Memory Device
-        Array Handle: 0x0005
-        Error Information Handle: Not Provided
-        Total Width: 64 bits
-        Data Width: 64 bits
-        Size: 4096 MB
-        Form Factor: SODIMM
-        Set: None
-        Locator: ChannelB-DIMM0
-        Bank Locator: BANK 2
-        Type: DDR3
-        Type Detail: Synchronous
-        Speed: 1333 MHz
-        Manufacturer: Samsung
-        Serial Number: 1234567
-        Asset Tag: 12345678910
-        Part Number: XYSADKLJADSL-132
-        Rank: Unknown
-    """)
+Handle 0x0007, DMI type 17, 28 bytes
+Memory Device
+    Array Handle: 0x0005
+    Error Information Handle: Not Provided
+    Total Width: 64 bits
+    Data Width: 64 bits
+    Size: 4096 MB
+    Form Factor: SODIMM
+    Set: None
+    Locator: ChannelB-DIMM0
+    Bank Locator: BANK 2
+    Type: DDR3
+    Type Detail: Synchronous
+    Speed: 1333 MHz
+    Manufacturer: Samsung
+    Serial Number: 1234567
+    Asset Tag: 12345678910
+    Part Number: XYSADKLJADSL-132
+    Rank: Unknown
+"""
 dmidecode_memory.expected_results = {
     'memory_total': 8192,
     'memory_devices_count': 2,
@@ -330,80 +327,79 @@ def dmidecode_processor(scan_result, stdout, stderr, succeeded):
 
         fetch_rows(scan_result, match, row_map, type_map=type_map)
 
+dmidecode_processor.mock_output = """
+# dmidecode 3.0
+Getting SMBIOS data from sysfs.
+SMBIOS 2.6 present.
 
-dmidecode_processor.mock_output = textwrap.dedent("""\
-    # dmidecode 3.0
-    Getting SMBIOS data from sysfs.
-    SMBIOS 2.6 present.
-
-    Handle 0x0001, DMI type 4, 42 bytes
-    Processor Information
-        Socket Designation: CPU
-        Type: Central Processor
-        Family: Core i7
-        Manufacturer: Intel(R) Corporation
-        ID: A7 06 02 00 FF FB EB BF
-        Signature: Type 0, Family 6, Model 42, Stepping 7
-        Flags:
-            FPU (Floating-point unit on-chip)
-            VME (Virtual mode extension)
-            DE (Debugging extension)
-            PSE (Page size extension)
-            TSC (Time stamp counter)
-            MSR (Model specific registers)
-            PAE (Physical address extension)
-            MCE (Machine check exception)
-            CX8 (CMPXCHG8 instruction supported)
-            APIC (On-chip APIC hardware supported)
-            SEP (Fast system call)
-            MTRR (Memory type range registers)
-            PGE (Page global enable)
-            MCA (Machine check architecture)
-            CMOV (Conditional move instruction supported)
-            PAT (Page attribute table)
-            PSE-36 (36-bit page size extension)
-            CLFSH (CLFLUSH instruction supported)
-            DS (Debug store)
-            ACPI (ACPI supported)
-            MMX (MMX technology supported)
-            FXSR (FXSAVE and FXSTOR instructions supported)
-            SSE (Streaming SIMD extensions)
-            SSE2 (Streaming SIMD extensions 2)
-            SS (Self-snoop)
-            HTT (Multi-threading)
-            TM (Thermal monitor supported)
-            PBE (Pending break enabled)
-        Version: Intel(R) Core(TM) i7-2620M CPU @ 2.70GHz
-        Voltage: 1.2 V
-        External Clock: 100 MHz
-        Max Speed: 2700 MHz
-        Current Speed: 2700 MHz
-        Status: Populated, Enabled
-        Upgrade: ZIF Socket
-        L1 Cache Handle: 0x0002
-        L2 Cache Handle: 0x0003
-        L3 Cache Handle: 0x0004
-        Serial Number: Not Supported by CPU
-        Asset Tag: None
-        Part Number: None
-        Core Count: 2
-        Core Enabled: 2
-        Thread Count: 4
-        Characteristics:
-            64-bit capable
+Handle 0x0001, DMI type 4, 42 bytes
+Processor Information
+    Socket Designation: CPU
+    Type: Central Processor
+    Family: Core i7
+    Manufacturer: Intel(R) Corporation
+    ID: A7 06 02 00 FF FB EB BF
+    Signature: Type 0, Family 6, Model 42, Stepping 7
+    Flags:
+        FPU (Floating-point unit on-chip)
+        VME (Virtual mode extension)
+        DE (Debugging extension)
+        PSE (Page size extension)
+        TSC (Time stamp counter)
+        MSR (Model specific registers)
+        PAE (Physical address extension)
+        MCE (Machine check exception)
+        CX8 (CMPXCHG8 instruction supported)
+        APIC (On-chip APIC hardware supported)
+        SEP (Fast system call)
+        MTRR (Memory type range registers)
+        PGE (Page global enable)
+        MCA (Machine check architecture)
+        CMOV (Conditional move instruction supported)
+        PAT (Page attribute table)
+        PSE-36 (36-bit page size extension)
+        CLFSH (CLFLUSH instruction supported)
+        DS (Debug store)
+        ACPI (ACPI supported)
+        MMX (MMX technology supported)
+        FXSR (FXSAVE and FXSTOR instructions supported)
+        SSE (Streaming SIMD extensions)
+        SSE2 (Streaming SIMD extensions 2)
+        SS (Self-snoop)
+        HTT (Multi-threading)
+        TM (Thermal monitor supported)
+        PBE (Pending break enabled)
+    Version: Intel(R) Core(TM) i7-2620M CPU @ 2.70GHz
+    Voltage: 1.2 V
+    External Clock: 100 MHz
+    Max Speed: 2700 MHz
+    Current Speed: 2700 MHz
+    Status: Populated, Enabled
+    Upgrade: ZIF Socket
+    L1 Cache Handle: 0x0002
+    L2 Cache Handle: 0x0003
+    L3 Cache Handle: 0x0004
+    Serial Number: Not Supported by CPU
+    Asset Tag: None
+    Part Number: None
+    Core Count: 2
+    Core Enabled: 2
+    Thread Count: 4
+    Characteristics:
+        64-bit capable
 
 
-    some bogus GPU...
+some bogus GPU...
 
-    Handle 0x0001, DMI type 4, 42 bytes
-    Processor Information
-        Socket Designation: GPU
-        Type: Graphics Unit
-        Family: Feline
-        Manufacturer: Cat Brains Inc
-        ID: 12 32 54 65 67
+Handle 0x0001, DMI type 4, 42 bytes
+Processor Information
+    Socket Designation: GPU
+    Type: Graphics Unit
+    Family: Feline
+    Manufacturer: Cat Brains Inc
+    ID: 12 32 54 65 67
 
-    """)
+"""
 dmidecode_processor.expected_results = {
     'processor_manufacturer': 'Intel(R) Corporation',
     'processor_family': 'Core i7',
--- lcrs_embedded/system/lspci.py
+++ lcrs_embedded/system/lspci.py
@@ -1,6 +1,5 @@
 import logging
 import re
-import textwrap
 
 from ..utils.decorators import run_command
 
@@ -40,27 +39,26 @@ def lspci_analysis(scan_result, stdout, stderr, succeeded):
         scan_result.usb_controller = m.group(1)
         scan_result.has_usb = True
 
-
-lspci_analysis.mock_output = textwrap.dedent("""\
-    00:00.0 Host bridge: Intel Corporation 2nd Generation Core Processor Family DRAM Controller (rev 09)
-    00:02.0 VGA compatible controller: Intel Corporation 2nd Generation Core Processor Family Integrated Graphics Controller (rev 09)
-    00:16.0 Communication controller: Intel Corporation 6 Series/C200 Series Chipset Family MEI Controller #1 (rev 04)
-    00:19.0 Ethernet controller: Intel Corporation 82579LM Gigabit Network Connection (rev 04)
-    00:1a.0 USB controller: Intel Corporation 6 Series/C200 Series Chipset Family USB Enhanced Host Controller #2 (rev 04)
-    00:1b.0 Audio device: Intel Corporation 6 Series/C200 Series Chipset Family High Definition Audio Controller (rev 04)
-    00:1c.0 PCI bridge: Intel Corporation 6 Series/C200 Series Chipset Family PCI Express Root Port 1 (rev b4)
-    00:1c.1 PCI bridge: Intel Corporation 6 Series/C200 Series Chipset Family PCI Express Root Port 2 (rev b4)
-    00:1c.3 PCI bridge: Intel Corporation 6 Series/C200 Series Chipset Family PCI Express Root Port 4 (rev b4)
-    00:1c.4 PCI bridge: Intel Corporation 6 Series/C200 Series Chipset Family PCI Express Root Port 5 (rev b4)
-    00:1c.6 PCI bridge: Intel Corporation 6 Series/C200 Series Chipset Family PCI Express Root Port 7 (rev b4)
-    00:1d.0 USB controller: Intel Corporation 6 Series/C200 Series Chipset Family USB Enhanced Host Controller #1 (rev 04)
-    00:1f.0 ISA bridge: Intel Corporation QM67 Express Chipset Family LPC Controller (rev 04)
-    00:1f.2 SATA controller: Intel Corporation 6 Series/C200 Series Chipset Family 6 port SATA AHCI Controller (rev 04)
-    00:1f.3 SMBus: Intel Corporation 6 Series/C200 Series Chipset Family SMBus Controller (rev 04)
-    03:00.0 Network controller: Intel Corporation Centrino Advanced-N 6205 [Taylor Peak] (rev 34)
-    0d:00.0 System peripheral: Ricoh Co Ltd PCIe SDXC/MMC Host Controller (rev 07)
-    0e:00.0 USB controller: NEC Corporation uPD720200 USB 3.0 Host Controller (rev 04)
-    """)  # noqa
+lspci_analysis.mock_output = """
+00:00.0 Host bridge: Intel Corporation 2nd Generation Core Processor Family DRAM Controller (rev 09)
+00:02.0 VGA compatible controller: Intel Corporation 2nd Generation Core Processor Family Integrated Graphics Controller (rev 09)
+00:16.0 Communication controller: Intel Corporation 6 Series/C200 Series Chipset Family MEI Controller #1 (rev 04)
+00:19.0 Ethernet controller: Intel Corporation 82579LM Gigabit Network Connection (rev 04)
+00:1a.0 USB controller: Intel Corporation 6 Series/C200 Series Chipset Family USB Enhanced Host Controller #2 (rev 04)
+00:1b.0 Audio device: Intel Corporation 6 Series/C200 Series Chipset Family High Definition Audio Controller (rev 04)
+00:1c.0 PCI bridge: Intel Corporation 6 Series/C200 Series Chipset Family PCI Express Root Port 1 (rev b4)
+00:1c.1 PCI bridge: Intel Corporation 6 Series/C200 Series Chipset Family PCI Express Root Port 2 (rev b4)
+00:1c.3 PCI bridge: Intel Corporation 6 Series/C200 Series Chipset Family PCI Express Root Port 4 (rev b4)
+00:1c.4 PCI bridge: Intel Corporation 6 Series/C200 Series Chipset Family PCI Express Root Port 5 (rev b4)
+00:1c.6 PCI bridge: Intel Corporation 6 Series/C200 Series Chipset Family PCI Express Root Port 7 (rev b4)
+00:1d.0 USB controller: Intel Corporation 6 Series/C200 Series Chipset Family USB Enhanced Host Controller #1 (rev 04)
+00:1f.0 ISA bridge: Intel Corporation QM67 Express Chipset Family LPC Controller (rev 04)
+00:1f.2 SATA controller: Intel Corporation 6 Series/C200 Series Chipset Family 6 port SATA AHCI Controller (rev 04)
+00:1f.3 SMBus: Intel Corporation 6 Series/C200 Series Chipset Family SMBus Controller (rev 04)
+03:00.0 Network controller: Intel Corporation Centrino Advanced-N 6205 [Taylor Peak] (rev 34)
+0d:00.0 System peripheral: Ricoh Co Ltd PCIe SDXC/MMC Host Controller (rev 07)
+0e:00.0 USB controller: NEC Corporation uPD720200 USB 3.0 Host Controller (rev 04)
+"""  # noqa
 
 lspci_analysis.expected_results = {
     'wifi': (
