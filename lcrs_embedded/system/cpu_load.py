cde1c382279bf23f63f0166ffd326913379d1c5c
Pieter Vancraeyveld <pieter.vancraeyveld@gmail.com>
2017-07-05 18:45:04 +0200

    Full PEP8 compliance

--- lcrs_embedded/system/cpu_load.py
+++ lcrs_embedded/system/cpu_load.py
@@ -2,10 +2,11 @@
 Generate a load on the CPU with one process per core.
 """
 import logging
-from multiprocessing import Process
 import time
+from multiprocessing import Process
 
 logger = logging.getLogger(__name__)
+
 
 def timed_cpu_load(endTime):
     """
@@ -20,30 +21,33 @@ def timed_cpu_load(endTime):
         m = (m + 1) % 100000
         running = (m != 0) or (time.time() < endTime)
 
+
 def cpu_load(scan_result, duration_in_seconds):
     """
     Load CPU.
     """
-    
+
     def multiple_processes_load_cpu(duration_in_seconds):
         """
         Start a number of processes that each do simple arithmetics to
         generate a load on the CPU for a number of seconds.
         """
         number_of_processes = scan_result.processor_cores
-        
+
         startTime = time.time()
         endTime = startTime + duration_in_seconds
-        logger.info("Starting to generate load on CPU, time {}".format(startTime))
+        logger.info(
+            "Starting to generate load on CPU, time {}".format(startTime))
         processes = []
         for n in range(number_of_processes):
-            process = Process(target = timed_cpu_load, args = (endTime,))
+            process = Process(target=timed_cpu_load, args=(endTime,))
             process.start()
             processes.append(process)
         for n in range(number_of_processes):
             processes[n].join()
-        logger.info("Finished generating load on CPU, time {}".format(time.time()))
-    
+        logger.info(
+            "Finished generating load on CPU, time {}".format(time.time()))
+
     try:
         multiple_processes_load_cpu(duration_in_seconds)
         scan_result.processor_load_status = "Complete"
